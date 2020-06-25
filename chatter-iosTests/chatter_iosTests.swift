c7f545386d4cbe84b568aeeb2cf369d40a0244ef
Julian Rubisch <julian@julianrubisch.at>
2020-06-25 09:06:39 +0200

    Initial Commit

---
+++ chatter-iosTests/chatter_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  chatter_iosTests.swift
+//  chatter-iosTests
+//
+//  Created by Julian Rubisch on 25.06.20.
+//  Copyright © 2020 Julian Rubisch. All rights reserved.
+//
+
+import XCTest
+@testable import chatter_ios
+
+class chatter_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
